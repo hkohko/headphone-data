460bbfa34bfbaef7df50c66d0389071ebfbe7ee3
heko <91366057+hkohko@users.noreply.github.com>
2023-10-24 21:49:37 +0700

    parsed product name and url

--- src/parse_data.py
+++ src/parse_data.py
@@ -1,14 +1,21 @@
 import json
+from collections import defaultdict
 
 from constants import JSON_FILES
 
 
-def parse_json() -> None:
+def parse_json() -> defaultdict[str, str]:
     with open(JSON_FILES.joinpath("sample_response.json")) as sample_file:
         file = json.load(sample_file)
     data = file.get("data")
     products = data.get("products")
-    print(len(products))
+    product_identity: defaultdict[str, str] = defaultdict(str)
+    for idx, product in enumerate(products):
+        name = product.get("fullname")
+        page = product.get("page")
+        url = page.get("url")
+        product_identity[name] = url
+    return product_identity
 
 
 if __name__ == "__main__":
