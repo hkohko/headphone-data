d0ed36f073e7d4caa877de9e41f9273460b35db7
heko <91366057+hkohko@users.noreply.github.com>
2023-10-24 11:25:34 +0700

    image url prep

--- src/open_image.py
+++ src/open_image.py
@@ -1,20 +1,17 @@
 from io import BytesIO
 
 import httpx
-from dotenv import dotenv_values
 from PIL import Image
 
-from constants import ENV
-
-key = dotenv_values(ENV)
-url: str = key.get("SAMPLE_IMAGE")
+from constants import SAMPLE_IMAGE
 
 
-def main():
-    link = httpx.get(url)
+def main() -> None:
+    link = httpx.get(SAMPLE_IMAGE)
     byte_img = BytesIO(link.content)
     img = Image.open(byte_img)
     img.show("sample")
+    img.close()
 
 
 if __name__ == "__main__":
