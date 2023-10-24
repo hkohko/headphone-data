a31aa668eb99b8b6568831d6aad83769e7c81f6b
heko <91366057+hkohko@users.noreply.github.com>
2023-10-24 11:26:19 +0700

    added url paths

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,11 @@
 from pathlib import Path
 
+from dotenv import dotenv_values
+
 PROJ_DIR = Path(__file__).parents[1]
 SRC = PROJ_DIR.joinpath("src")
 ENV = PROJ_DIR.joinpath(".env")
+
+SAMPLE_IMAGE = dotenv_values(ENV).get("SAMPLE_IMAGE")
+SAMPLE_URL = dotenv_values(ENV).get("SAMPLE_URL")
+LINKS_URL = dotenv_values(ENV).get("LINKS_URL")
