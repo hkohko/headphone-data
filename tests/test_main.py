2afbd7e4aa17c81b671fc505d2dfc91e73249540
heko <91366057+hkohko@users.noreply.github.com>
2023-10-25 10:56:20 +0700

    placeholder test

---
+++ tests/test_main.py
@@ -0,0 +1,32 @@
+import json
+from pathlib import Path
+
+from constants import JSON_FILES
+from src.data.sql_query import connect
+
+
+def open_file():
+    filepaths: list[Path] = []
+    for file in Path(JSON_FILES).iterdir():
+        filepaths.append(file)
+    print(filepaths)
+    for path in filepaths:
+        if path.suffix != "json":
+            continue
+        with open(path) as jsonfile:
+            json.load(jsonfile)
+        print(path)
+
+
+def query_db():
+    Q_TEST_SELECT = "SELECT name, url FROM headphone ORDER BY name ASC LIMIT 10"
+    with connect() as conn:
+        cur = conn.cursor()
+        cur.execute(Q_TEST_SELECT)
+        for result in cur:
+            print(result)
+
+
+if __name__ == "__main__":
+    open_file()
+    query_db()
