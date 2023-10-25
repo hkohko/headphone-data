db6e741e338e10ecb14074a6b65c859d70efe5cb
heko <91366057+hkohko@users.noreply.github.com>
2023-10-25 10:53:37 +0700

    updated DB constants

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,7 @@ PROJ_DIR = PurePath(__file__).parents[1]
 SRC = PROJ_DIR.joinpath("src")
 ENV = PROJ_DIR.joinpath(".env")
 JSON_FILES = SRC.joinpath("data", "json_files")
-DB = SRC.joinpath("data", "db", "headphones.db")
+DB_DIR = SRC.joinpath("data", "db")
 
 SAMPLE_IMAGE = dotenv_values(ENV).get("SAMPLE_IMAGE")
 SAMPLE_URL = dotenv_values(ENV).get("SAMPLE_URL")
@@ -14,6 +14,8 @@ LINKS_URL = dotenv_values(ENV).get("LINKS_URL")
 
 PRODUCT_IDENT_NAME = "product_identity.json"
 SAMPLE_RESPONSE_NAME = "sample_response.json"
+DB_NAME = "headphones.db"
 
+DB = DB_DIR.joinpath(DB_NAME)
 SAMPLE_RESPONSE_PATH = JSON_FILES.joinpath(SAMPLE_RESPONSE_NAME)
 PRODUCT_IDENT_PATH = JSON_FILES.joinpath(PRODUCT_IDENT_NAME)
