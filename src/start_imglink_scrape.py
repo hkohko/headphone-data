adcf45ae7805e4186a03ffcf8cc4bd030e034370
heko <91366057+hkohko@users.noreply.github.com>
2023-10-26 15:59:12 +0700

    run scrapes concurrently

--- src/start_imglink_scrape.py
+++ src/start_imglink_scrape.py
@@ -6,11 +6,13 @@ from frequency_graph_scraper import create_link_to_product_page, task
 
 
 async def main(func, img_size):
+    tasks = []
     id_and_links = await create_link_to_product_page()
     for content in tqdm(id_and_links):
         db_id = content[0]
         link = content[1]
-        await func(link, db_id, img_size)
+        tasks.append(asyncio.create_task(func(link, db_id, img_size)))
+    await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
